a9ba7610bdf2539180d78822f713545bd3b9f431
zapccu <d.braner@gmx.net>
2021-12-07 18:19:41 +0100

    Fixed GetUpdate for HMCCUDEV

--- FHEM/88_HMCCUCHN.pm
+++ FHEM/88_HMCCUCHN.pm
@@ -30,7 +30,7 @@ sub HMCCUCHN_Set ($@);
 sub HMCCUCHN_Get ($@);
 sub HMCCUCHN_Attr ($@);
 
-my $HMCCUCHN_VERSION = '5.0 213381928';
+my $HMCCUCHN_VERSION = '5.0 213401910';
 
 ######################################################################
 # Initialize module
