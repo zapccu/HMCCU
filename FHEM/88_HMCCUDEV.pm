0eec9c61eac929245bb849a5c000ff379feaa5d0
zapccu <d.braner@gmx.net>
2022-01-25 18:47:44 +0100

    RPC Multicall support

--- FHEM/88_HMCCUDEV.pm
+++ FHEM/88_HMCCUDEV.pm
@@ -31,7 +31,7 @@ sub HMCCUDEV_Set ($@);
 sub HMCCUDEV_Get ($@);
 sub HMCCUDEV_Attr ($@);
 
-my $HMCCUDEV_VERSION = '5.0 220061807';
+my $HMCCUDEV_VERSION = '5.0 220251847';
 
 ######################################################################
 # Initialize module
