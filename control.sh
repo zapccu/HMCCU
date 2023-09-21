f2f21b5c2cc72a0354582fd6a1d608ce3e9e0254
zapccu <d.braner@gmx.net>
2023-09-21 19:29:34 +0200

    Fixed control file

--- control.sh
+++ control.sh
@@ -6,8 +6,9 @@ rm $CONTROL
 
 echo "Creating FHEM update control file $CONTROL ..."
 
-find $DIRS -type f \( ! -iname ".*" \) -print0 | while IFS= read -r -d '' f; 
+find $DIRS -name *pm -type f \( ! -iname ".*" \) -print0 | while IFS= read -r -d '' f; 
 do
+	echo "Processing $f"
 	out="UPD `stat -f "%Sm %z" -t "%Y-%m-%d_%H:%M:%S" $f` $f"
 	echo ${out//.\//} >> $CONTROL
 done
